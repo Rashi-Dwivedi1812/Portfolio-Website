49433bfe39085d1b69a58c6527820bb164ce9559
Rashi-Dwivedi1812 <rashidwivedi1812@gmail.com>
2025-07-03 18:59:58 +0530

    Navbar created

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Navbar } from './components/Navbar/Navbar';
+import { Skills } from './components/skills/Skills';
+import { About } from './components/about/About';
+import { Experience } from './components/experience/Experience';
+import { Work } from './components/work/Work';
+import { Education } from './components/education/Education';
+import { Contact } from './components/contact/Contact';
+import { Footer } from './components/footer/Footer';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
+    <div className='bg-[#050414]'>
+       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Navbar/>
+        <About/>
+        <Skills/>
+        <Experience/>
+        <Work/>
+        <Education/>
+        <Contact/>
+        <Footer/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
